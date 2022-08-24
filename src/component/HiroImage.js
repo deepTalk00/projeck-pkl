0dbf6c08cb42b2e775298a54d4d4c4bb293d5b07
deepTalk00 <you@example.com>
2022-08-25 06:49:41 +0700

    hiro-header

--- src/component/HiroImage.js
+++ src/component/HiroImage.js
@@ -5,12 +5,12 @@ function HiroImage() {
      return (
        <div className="container_hiro w-screen h-screen ">
         <div className='w-screen flex justify-center -space-x-[100vw]'>
-          <img src ={Hiro} alt="gambar header" className='mix-blend-screen bg-slate-400 h-screen object-cover  '/>
+          <img src ={Hiro} alt="gambar header" className='mix-blend-screen bg-slate-400 h-[90vh] object-cover  '/>
           <div className="w-screen mix-blend-multiply bg-slate-500"></div>
           </div>
-        <secsion className="w-3/4  mx-14  bg-slate-500 h-[22em] flex flex-col justify-between items-start absolute top-0 translate-y-3/4"> 
-          <header className="text-white text-5xl text-left font-serif capitalize leading-tight">Mengurus surat izin di BAKESBANGPOL Kota Binjai</header>
-          <button className="px-10 py-2 bg-greentea rounded-2xl text-white uppercase font-serif">Pergi</button>
+        <secsion className="w-3/4  mx-11 h-[20em] flex flex-col justify-between items-start absolute top-0 translate-y-3/4"> 
+          <header className="header_hiro text-white text-5xl text-left font-serif capitalize leading-tight">Mengurus surat izin di <span className='text-greentea'> BAKESBANGPOL</span> Kota Binjai</header>
+          <button className="px-10 py-2 bg-greentea rounded-2xl shadow-xl drop-shadow-xl hover:ring-1 hover:ring-white focus:ring-2 animate-bounce text-white uppercase font-serif">Pergi</button>
         </secsion>
        </div>
      );
